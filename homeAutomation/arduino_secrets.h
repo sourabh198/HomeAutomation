b87d04ab77a9266ec6a6bf84be4eb67f074476fd
sourabh198 <sou2sms@gmail.com>
2021-08-06 22:02:51 +0530

    Sample Program Added
    A pre final Arduino Sketch which will help us with the end product

---
+++ homeAutomation/arduino_secrets.h
@@ -0,0 +1,3 @@
+#define SECRET_SSID "TP-LINK_82E6"  //Enter WIFI NAME
+#define SECRET_PASS "shubhsou@1316"  //Enter WIFI PASSWORD
+#define SECRET_DEVICE_KEY "M496DX6FM7BVFTULRMN4"  //Enter Secret Key
